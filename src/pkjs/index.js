b33d63139f3ad31a103a3e59b9b1b7e3f278b2a3
Danielle Tomlinson <dan@tomlinson.io>
2016-10-23 11:27:15 +0100

    Pull data from API

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,10 +1,7 @@
-var Pusher = require('./pusher');
-
 function fetch() {
-  //var req = new XMLHttpRequest();
-  /*
+  var req = new XMLHttpRequest();
   // build the GET request
-  req.open('GET', 'http://dev.markitondemand.com/Api/Quote/json?symbol=' + symbol, true);
+  req.open('GET', "http://545f9eab.ngrok.io/messages?for=4916092192757", true);
   req.onload = function(e) {
     if (req.readyState == 4) {
       // 200 - HTTP OK
@@ -12,31 +9,17 @@ function fetch() {
         console.log(req.responseText);
         
         var response = JSON.parse(req.responseText);
-        if (response['Message']) {
-          // the markitondemand API sends a response with a Message
-          // field when the symbol is not found
-          Pebble.sendAppMessage({ 'QuoteKeyPrice': 'Not Found' });
-        }
-        if (response['Data']) {
-          // data found, look for LastPrice
-          var price = response['Data']['LastPrice'];
-          console.log('Price is ' + price);
-
-          Pebble.sendAppMessage({
-            'YourKey': true
-          }, function(e) {
-            console.log('sent');
-          }, function() {
-            console.log('failed');
-          });
-        }
+        response.forEach(function(item) {
+          if (item['message']) {
+            Pebble.sendAppMessage({ 'location': item['message'], 'from', item['from'] });
+          }
+        })
       } else {
         console.log('Request returned error code ' + req.status.toString());
       }
     }
   };
   req.send(null);
-  */
 }
 
 // Set callback for the app ready event
@@ -53,31 +36,5 @@ Pebble.addEventListener('ready', function(e) {
   }, function() {
     console.log('failed');
   });
-  
-  
-pusherTest();
-  
 });
 
-
-function pusherTest() {
-  // Enable pusher logging - don't include this in production
-    Pusher.logToConsole = true;
-
-    var pusher = new Pusher('f0c2cfa5ebd364b6521b', {
-      cluster: 'eu',
-      encrypted: true
-    });
-
-    var channel = pusher.subscribe('test_channel');
-    channel.bind('my_event', function(data) {
-      console.log(data.message);
-    });
-}
-
-// Set callback for appmessage events
-Pebble.addEventListener('appmessage', function(e) {
-  console.log('message');
-  //Receive from watch
-  fetch();
-});
